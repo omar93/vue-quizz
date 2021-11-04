12d8773f4a6ec5aeea32a9b32f926791ad1a6c1c
Omar Alhamad <omar93@hotmail.se>
2021-11-04 23:26:40 +0100

    component looping working, get props to work

---
+++ vue-quizz/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <ul id="questions">
+  <li 
+  v-for="question in questions" 
+  :key="question.question"
+  >
+<Question question/>
+  </li>
+</ul>
+  
+</div>
+</template>
+
+<script>
+import Question from './components/Question.vue'
+export default {
+  name: 'App',
+  components: {
+    Question
+  },
+  data() {
+
+  return {
+    questions: [
+      {
+        question:"who is batman",
+        alternatives:["batman", "gemberss","horunge", "turk"],
+        answear:"batman"
+      },
+      {
+        question:"who is batman",
+        alternatives:["batman", "gemberss","horunge", "turk"],
+        answear:"batman"
+      },
+    ]
+  }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue-quizz/src/components/Question.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="question-container">
+        <span id="question-title">Who is batman</span>
+        <ul id="answear-list">
+            <li> <button @click="handleClick">batman</button> </li>
+            <li> <button @click="handleClick">iron man</button> </li>
+            <li> <button @click="handleClick">the flash</button> </li>
+            <li> <button @click="handleClick">superman</button> </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Array
+  },
+  methods: {
+      handleClick() {
+          alert('kebab')
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+div {
+    display: flex;
+    flex-direction: column;
+}
+
+
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    margin-top: 20px;
+}
+</style>
